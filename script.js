ed3ca2d30729b32b8df1d12aa191b0f261708672
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-03-24 22:46:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+let transactions = JSON.parse(localStorage.getItem("data")) || [];
+
+function updateUI() {
+  let list = document.getElementById("list");
+  let balance = document.getElementById("balance");
+
+  list.innerHTML = "";
+
+  let total = 0;
+
+  transactions.forEach((t, index) => {
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+      ${t.desc} - ₹${t.amount} (${t.type})
+      <button onclick="deleteItem(${index})">❌</button>
+    `;
+
+    list.appendChild(li);
+
+    if (t.type === "income") {
+      total += t.amount;
+    } else {
+      total -= t.amount;
+    }
+  });
+
+  balance.innerText = total;
+}
+
+function addTransaction() {
+  let desc = document.getElementById("desc").value;
+  let amount = Number(document.getElementById("amount").value);
+  let type = document.getElementById("type").value;
+  let source = document.getElementById("source").value;
+
+  if (desc === "" || amount === 0) {
+    alert("Enter valid data");
+    return;
+  }
+
+  transactions.push({ desc, amount, type, source });
+
+  localStorage.setItem("data", JSON.stringify(transactions));
+
+  updateUI();
+
+  document.getElementById("desc").value = "";
+  document.getElementById("amount").value = "";
+}
+
+function deleteItem(index) {
+  transactions.splice(index, 1);
+  localStorage.setItem("data", JSON.stringify(transactions));
+  updateUI();
+}
+
+updateUI();
