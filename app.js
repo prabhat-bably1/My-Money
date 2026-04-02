a03ba4dfbc09c8cc089535e1d5fb6327ff08d45a
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-04-02 21:40:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -82,3 +82,16 @@ function logout(){
 if(location.pathname.includes("dashboard")){
   loadData();
 }
+function downloadPDF(){
+  let content = "Money Report\n\n";
+
+  document.querySelectorAll("#list li").forEach(li=>{
+    content += li.innerText + "\n\n";
+  });
+
+  const blob = new Blob([content], {type: "text/plain"});
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = "report.txt";
+  a.click();
+}
