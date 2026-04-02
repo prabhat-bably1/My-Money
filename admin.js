e4590f32cb49584935536b1f64ba7baa5dda7b12
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-04-02 14:27:36 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,7 +1,6 @@
-function makeUserAdmin() {
-  let user = JSON.parse(localStorage.getItem("user"))
-  user.role = "admin"
-  localStorage.setItem("user", JSON.stringify(user))
-
-  alert("Now you are admin")
+function makeAdmin() {
+  let user = JSON.parse(localStorage.getItem("user"));
+  user.role = "admin";
+  localStorage.setItem("user", JSON.stringify(user));
+  alert("You are now admin");
 }
