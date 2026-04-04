50b4afbd6af19cab2320621188bf228e3a24ef07
prabhat-bably1 <prabhatr.seth04@gmail.com>
2026-04-04 21:20:09 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -81,3 +81,57 @@ function logout(){
 }
 
 loadData();
+
+function calculateTax(income){
+  let tax = 0;
+
+  if(income <= 250000) tax = 0;
+  else if(income <= 500000) tax = (income - 250000) * 0.05;
+  else if(income <= 1000000) tax = 12500 + (income - 500000) * 0.2;
+  else tax = 112500 + (income - 1000000) * 0.3;
+
+  let caCharge = tax > 0 ? tax * 0.05 : 0;
+
+  return {
+    tax: Math.round(tax),
+    caCharge: Math.round(caCharge)
+  };
+}
+
+function showTaxDetails(){
+
+  const box = document.getElementById("taxDetails");
+
+  if(box.style.display === "block"){
+    box.style.display = "none";
+    return;
+  } else {
+    box.style.display = "block";
+  }
+
+  let income = 0;
+  let expense = 0;
+
+  allData.forEach(d => {
+    if(d.type === "income") income += Number(d.amount);
+    else expense += Number(d.amount);
+  });
+
+  let balance = income - expense;
+  if(balance < 0) balance = 0;
+
+  const result = calculateTax(balance);
+
+  document.getElementById("taxIncome").innerText = "₹" + income;
+  document.getElementById("taxExpense").innerText = "₹" + expense;
+  document.getElementById("taxBalance").innerText = "₹" + balance;
+
+  if(balance <= 250000){
+    document.getElementById("taxAmount").innerText = "No Tax";
+    document.getElementById("taxAdvice").innerText = "Safe 👍";
+    return;
+  }
+
+  document.getElementById("taxAmount").innerText = "Tax: ₹" + result.tax;
+  document.getElementById("caCharge").innerText = "CA: ₹" + result.caCharge;
+}
